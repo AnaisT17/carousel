a9e2e8f91408d8ebc9b468c44c65ec10b4dd2278
AnaisT17 <tchicayanais@gmail.com>
2019-09-28 21:42:22 +0200

    add button functions on file script.js on folder css-js

--- css-js/script.js
+++ css-js/script.js
@@ -1,2 +1,11 @@
-variable const ou let avec queryselector
-function appeler des fonctions 
+const buttonRight = document.querySelector(".button-right");
+const buttonLeft = document.querySelector(".button-left");
+const li = document.querySelectorAll("li");
+let i = 0;
+
+function buttonNext () {
+    li[i].classList.remove("active");
+    i++;
+    if (i==li.length)
+    {i = 0}
+}
