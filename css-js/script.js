0a6d671be2782b95610115d0519a4a93e6dcd68e
AnaisT17 <tchicayanais@gmail.com>
2019-09-29 10:06:34 +0200

    add line 1 use stricton file script.js on folder css-js

--- css-js/script.js
+++ css-js/script.js
@@ -1,3 +1,5 @@
+`use strict`;
+
 const buttonRight = document.querySelector(".button-right");
 const buttonLeft = document.querySelector(".button-left");
 const li = document.querySelectorAll("li");
@@ -10,4 +12,13 @@ function buttonNext () {
     {i = 0}
     li[i].classList.add("active");
 }
+buttonRight.addEventListener ("click", buttonNext);
 
+function buttonPrevious () {
+    li[i].classList.remove("active");
+    i--;
+    if (i < 0 )
+    {i = li.length -1}
+    li[i].classList.add("active");
+}
+buttonLeft.addEventListener ("click", buttonPrevious);
